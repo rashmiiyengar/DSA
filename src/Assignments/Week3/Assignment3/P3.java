d9034b1bb11f95b519524e53c8188acaf4d4c726
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-06-10 16:45:29 -0700

    Program that exchanges the values of first half side elements with second half side elements of array.

--- src/Assignments/Week3/Assignment3/P3.java
+++ src/Assignments/Week3/Assignment3/P3.java
@@ -1,5 +1,6 @@
 package Assignments.Week3.Assignment3;
 
+import java.util.Scanner;
 
 //Program that exchanges the values of first half side elements with second half side elements of array.
 
@@ -8,8 +9,34 @@ public class P3 {
     
 
     public static void main(String[] args) {
-            
 
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Size of Array: ");
+        int size=sc.nextInt();
+        int a[]=new int[size];
+        System.out.println("\nEnter Values in Array");
+        int i;
+        for(i=0;i<size;i++)
+        {
+            System.out.print("Enter Value: ");
+            a[i]=sc.nextInt();
+        }
+
+        int k=size%2==1?size/2+1:size/2;
+        System.out.println(k);
+        
+        for(i=0;i<size/2;i++)
+        {
+            int t=a[i];
+            a[i]=a[k];
+            a[k]=t;
+            k++;
+        }
+        System.out.println();
+        for(i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
 
     }
 }
---
+++ src/Assignments/Week3/Assignment3/P4.java
@@ -0,0 +1,30 @@
+package Assignments.Week3.Assignment3;
+
+import java.util.Scanner;
+
+public class P4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("eneter values of array");
+        int i;
+
+        for (i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (i = 0; i < size; i++) {
+            if (arr[i] != arr[0]) {
+                break;
+            }
+        }
+
+        if (i == size)
+            System.out.println("elements are equal");
+        else
+            System.out.println("elements are not equal");
+    }
+}
