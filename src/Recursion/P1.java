52b13589af1173b0f5c0abe136edee0c72603354
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-06-12 09:57:55 -0700

    recursion

--- src/Recursion/P1.java
+++ src/Recursion/P1.java
@@ -1,17 +1,18 @@
 package Recursion;
 
+//Time complexity O(n)
+//Space Complexity O(n)
 public class P1 {
 
     public void display(int x){
-
 
         if(x>5)
             return;
         else   
             System.out.println(x);
             display(x+1);
-
     }
+
     public static void main(String[] args) {
         
         P1 p= new P1();
