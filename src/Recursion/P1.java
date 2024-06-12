a8e733d9d9f9228ef558b3ca21779d905997849b
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-06-12 09:25:33 -0700

    recursion

---
+++ src/Recursion/P1.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class P1 {
+
+    public void display(int x){
+
+
+        if(x>5)
+            return;
+        else   
+            System.out.println(x);
+            display(x+1);
+
+    }
+    public static void main(String[] args) {
+        
+        P1 p= new P1();
+        p.display(1);
+
+    }
+}
