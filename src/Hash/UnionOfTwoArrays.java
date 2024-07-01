c968f93c38f58848d51a89d5325ed05f87760ad2
rashmisoundar <rashmiiyengar255@gmail.com>
2024-07-01 11:18:30 -0700

    union of two array using hasset

---
+++ src/Hash/UnionOfTwoArrays.java
@@ -0,0 +1,33 @@
+package Hash;
+
+import java.util.HashSet;
+
+// Union {7,3,9,6,2,4}
+// Nested noop - Time complexity will be o(n^2)
+// Sort o(nlogn)
+// optimal using hashmap
+
+public class UnionOfTwoArrays {
+    
+    public static int union(int arr1[], int arr2[]){
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+        for(int j=0;j<arr2.length;j++){
+            set.add(arr2[j]);
+        }
+        return set.size();
+    }
+
+    public static void main(String[] args) {
+        
+        int arr1[] = {7,3,9};
+        int arr2[] = {6,3,9,2,9,4};
+
+        int setSize =union(arr1, arr2);
+        System.out.println("Number of elements in union set is : " +setSize);
+        
+    }
+}
