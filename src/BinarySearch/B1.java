0bfd80142b9967e272180249eec3b1fc9b859aa3
rashmisoundar <rashmiiyengar255@gmail.com>
2024-07-22 15:42:06 -0700

    Linked list

---
+++ src/BinarySearch/B1.java
@@ -0,0 +1,11 @@
+package BinarySearch;
+
+public class B1 {
+
+    public static void main(String[] args) {
+        
+
+        
+    }
+    
+}
---
+++ src/LinkedList/P1.java
@@ -0,0 +1,60 @@
+package LinkedList;
+
+import java.lang.reflect.Method;
+
+//Memory allocated - non contiguous
+//Variable size
+//use whenever insert or delete at index , Time complexity O(1)
+//search time complexity is O(N) 
+
+//
+
+public class P1 {
+
+    class Node{
+
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next= null;
+        }
+    }
+    // head of the list
+    private static Node head;
+
+    // Method to print the linked list
+    public static void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+   
+
+    //add - first,last
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+       
+        if(head== null){
+             head=  newNode;
+             return;
+        }
+        newNode.next = head;
+        head= newNode;
+    
+    }
+
+    public static void main(String[] args) {
+
+        P1 list = new P1();
+            list.addFirst("a");
+            list.addFirst("b");
+
+            printList();
+    }
+    
+}
