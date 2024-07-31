8f3612f0185929f0caa611eacd6a33b6a6808d6a
rashmisoundar <rashmiiyengar255@gmail.com>
2024-07-30 17:44:23 -0700

    RunningSums1480

---
+++ src/Leetcode/RunningSums1480.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+public class RunningSums1480 {
+    public static int[] runningSum(int[] nums) {
+        
+        for(int i=1;i<nums.length;i++){
+                nums[i] = nums[i-1]+nums[i];
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4};
+
+        int[] res=runningSum(nums);
+
+        for(int i=0;i<res.length;i++){
+            System.out.print(res[i]+ " ");
+        }
+
+    }
+}
